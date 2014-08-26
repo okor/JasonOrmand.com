595c133d521f29afa829d80bbec131d6b73744c3
okor <jason.ormand1@gmail.com>
2014-08-26 10:01:25 -0400

    y u no work

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 working_directory "/home/deployer/sites/jo/current"
 pid "/home/deployer/sites/jo/shared/pids/unicorn.pid"
 stderr_path "/home/deployer/sites/jo/shared/log/unicorn.log"
-stdout_path "/home/deployer/sites/jo/shared/log/unicorn.log"
+stdout_path "/home/deployer/sites/jo/shared/log/unicorn.err.log"
 
 listen "/tmp/unicorn.jo.sock"
 listen 1336
